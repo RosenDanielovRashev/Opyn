d785ccca293fe1a6f59fcd34cf701031f0ccf650
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-01 15:49:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
 import pandas as pd
-import plotly.graph_objects as go
+import plotly.express as px
 
-# Зареждане на CSV файла
-df = pd.read_csv("danni.csv")  # Увери се, че името на файла съвпада
+df = pd.read_csv("danni.csv")
 
-# Pivot таблица: редове = y, колони = H/D, стойности = Ei/Ed
-pivot_table = df.pivot_table(index='y', columns='H/D', values='Ei/Ed')
-
-# Създаване на контурна графика
-fig = go.Figure(data=
-    go.Contour(
-        z=pivot_table.values,
-        x=pivot_table.columns,
-        y=pivot_table.index,
-        colorscale='Viridis',
-        contours=dict(
-            coloring='heatmap',
-            showlabels=True
-        ),
-        colorbar=dict(title='Ei/Ed')
-    )
-)
-
-fig.update_layout(
-    title='Контурна графика на Ei/Ed спрямо y и H/D',
-    xaxis_title='H/D',
-    yaxis_title='y'
+fig = px.scatter(
+    df,
+    x='H/D',
+    y='y',
+    color='Ei/Ed',
+    color_continuous_scale='Viridis',
+    title='Scatter plot на Ei/Ed спрямо y и H/D',
+    labels={'H/D':'H/D', 'y':'y', 'Ei/Ed':'Ei/Ed'}
 )
 
 fig.show()
