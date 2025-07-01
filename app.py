10f8c3ee12ddc25d5e6fc4abe5e02cf2f97ecd30
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-01 16:54:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,20 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-st.title("Изолинии по подадени точки с контрол на мащаба по осите")
+st.title("Изолинии с реално съотношение 1:1")
 
-# Зареждане на CSV
+# Зареждане на данни
 df = pd.read_csv("danni.csv")
 
-# Показване на примерни данни
+# Показване на данни
 st.write("Примерни данни:", df.head())
 
-# Вземаме уникалните стойности на Ei/Ed
 unique_levels = sorted(df['Ei/Ed'].unique())
 
 fig = go.Figure()
 
 for level in unique_levels:
     df_level = df[df['Ei/Ed'] == level].sort_values(by='H/D')
-
     fig.add_trace(go.Scatter(
         x=df_level['H/D'],
         y=df_level['y'],
@@ -29,13 +27,14 @@ for level in unique_levels:
 fig.update_layout(
     xaxis=dict(
         title='H/D',
-        dtick=0.1  # мащаб по x
+        dtick=0.1
     ),
     yaxis=dict(
         title='y',
-        dtick=0.1  # мащаб по y
+        dtick=0.1,
+        scaleanchor='x'  # Скалата по y е свързана с тази по x
     ),
-    title='Изолинии на Ei/Ed с фиксиран мащаб по осите',
+    title='Изолинии с реален мащаб 1:1',
     legend=dict(title='Легенда')
 )
 
