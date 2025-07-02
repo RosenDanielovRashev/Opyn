d7a52e1d56059c25f9804333e51884830419b3aa
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-02 11:36:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-st.title("Изолинии с две хоризонтални оси")
+st.title("Комбинирани изолинии")
 
-# Зареждане на данни
+# Зареждане на оригиналните данни
 df_original = pd.read_csv("danni.csv")
+
+# Зареждане на новите данни
 df_new = pd.read_csv("Оразмеряване на опън за междиннен плстH_D.csv")
-df_new.rename(columns={'?sr/Ei': 'sigma_r'}, inplace=True)
+df_new.rename(columns={'Esr/Ei': 'sr_Ei'}, inplace=True)
 
 # Създаване на фигура
 fig = go.Figure()
 
-# 1. Оригинални изолинии (Ei/Ed)
+# 1. Добавяне на оригиналните изолинии за Ei/Ed
 if 'Ei/Ed' in df_original.columns:
     unique_Ei_Ed = sorted(df_original['Ei/Ed'].unique())
     for level in unique_Ei_Ed:
@@ -22,67 +24,41 @@ if 'Ei/Ed' in df_original.columns:
             y=df_level['y'],
             mode='lines',
             name=f'Ei/Ed = {level}',
-            line=dict(width=2),
-            xaxis='x1'
+            line=dict(width=2)
         ))
 
-# 2. Нови изолинии (σr/Ei)
-if 'sigma_r' in df_new.columns:
-    unique_sigma_r = sorted(df_new['sigma_r'].unique())
-    for sigma in unique_sigma_r:
-        df_level = df_new[df_new['sigma_r'] == sigma].sort_values(by='H/D')
-        x_normalized = df_level['H/D'] / 2
+# 2. Добавяне на новите изолинии за sr_Ei
+if 'sr_Ei' in df_new.columns:
+    unique_sr_Ei = sorted(df_new['sr_Ei'].unique())
+    for sr_Ei in unique_sr_Ei:
+        df_level = df_new[df_new['sr_Ei'] == sr_Ei].sort_values(by='H/D')
         fig.add_trace(go.Scatter(
-            x=x_normalized,
+            x=df_level['H/D'],
             y=df_level['y'],
             mode='lines',
-            name=f'σr/Ei = {sigma}',
-            line=dict(width=2),
-            xaxis='x2'
+            name=f'σsr/Ei = {sr_Ei}',
+            line=dict(width=2)
         ))
 
-# Настройки на графиките
+# Настройки на графиката (както в оригиналния код)
 fig.update_layout(
     width=700,
     height=700,
-    # Първа ос X (горна)
     xaxis=dict(
         title='H/D',
-        dtick=0.2,
-        range=[0, 2],
-        anchor='y',
-        position=1.0,  # Точна позиция
-        showgrid=True
-    ),
-    # Втора ос X (долна)
-    xaxis2=dict(
-        title='σr/Ei',
         dtick=0.1,
-        range=[0, 1],
-        overlaying='x',
-        side='bottom',
-        anchor='free',
-        position=0.0,  # Точна позиция под графиката
-        showgrid=False
+        range=[0, 2],
+        constrain='domain'
     ),
     yaxis=dict(
         title='y',
         dtick=0.1,
         range=[0, 2.5],
         scaleanchor='x',
-        scaleratio=1,
-        domain=[0.0, 0.95]  # Оставя място за долната ос
+        scaleratio=1
     ),
-    title='Изолинии с две хоризонтални оси',
-    legend=dict(
-        title='Легенда',
-        orientation="h",
-        yanchor="bottom",
-        y=1.02,
-        xanchor="right",
-        x=1
-    ),
-    margin=dict(b=60)  # Добавя място за долната ос
+    title='Комбинирани изолинии',
+    legend=dict(title='Легенда')
 )
 
 st.plotly_chart(fig, use_container_width=False)
