20b3be3cee7ed19f3482b8f779457071a0a3e62a
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-02 12:18:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ df_original = pd.read_csv("danni.csv")
 df_new = pd.read_csv("Оразмеряване на опън за междиннен плстH_D.csv")
 df_new.rename(columns={'Esr/Ei': 'sr_Ei'}, inplace=True)
 
-# Създаване на фигура
 fig = go.Figure()
 
-# 1. Добавяне на оригиналните изолинии за Ei/Ed
+# Добавяне на оригиналните изолинии за Ei/Ed
 if 'Ei/Ed' in df_original.columns:
     unique_Ei_Ed = sorted(df_original['Ei/Ed'].unique())
     for level in unique_Ei_Ed:
@@ -27,7 +26,7 @@ if 'Ei/Ed' in df_original.columns:
             line=dict(width=2)
         ))
 
-# 2. Добавяне на новите изолинии за sr_Ei
+# Добавяне на новите изолинии за sr_Ei
 if 'sr_Ei' in df_new.columns:
     unique_sr_Ei = sorted(df_new['sr_Ei'].unique())
     for sr_Ei in unique_sr_Ei:
@@ -40,17 +39,16 @@ if 'sr_Ei' in df_new.columns:
             line=dict(width=2)
         ))
 
-# Добавяне на празна следа за визуализиране на горната ос (xaxis2)
+# Празна следа за горната ос (xaxis2)
 fig.add_trace(go.Scatter(
-    x=[0, 1],
-    y=[None, None],
+    x=[0,1],
+    y=[None,None],
     mode='lines',
     xaxis='x2',
     showlegend=False,
     hoverinfo='skip'
 ))
 
-# Настройки на графиката с добавена горна ос с LaTeX заглавие
 fig.update_layout(
     width=700,
     height=700,
@@ -62,7 +60,7 @@ fig.update_layout(
         domain=[0, 1]
     ),
     xaxis2=dict(
-        title=r'$\sigma_r$',  # <-- LaTeX заглавие на новата ос
+        title='σₙ',   # Ето тук е името на новата ос с unicode индекс n
         overlaying='x',
         side='top',
         range=[0, 1],
@@ -82,5 +80,4 @@ fig.update_layout(
     legend=dict(title='Легенда')
 )
 
-# Показване на графиката
 st.plotly_chart(fig, use_container_width=False)
