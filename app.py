9d5a82a0abdc18b98a056e0c5907843e8a8f5ade
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-03 10:43:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -295,3 +295,9 @@ fig.update_layout(
     showlegend=False
 )
 st.plotly_chart(fig)
+# Проверка дали x_interp_EiEd е дефинирана и не е None
+if ('x_interp_EiEd' in locals()) and (x_interp_EiEd is not None):
+    sigma_r = (x_interp_EiEd)/2
+    st.markdown(f"**σr = {sigma_r}**")
+else:
+    st.markdown("**σr = -** (Няма изчислена стойност)")
