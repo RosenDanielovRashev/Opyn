214a32c16c1b54f61313aec96f0f34ca2e06e268
RosenDanielovRashev <rosenrashev98@gmail.com>
2025-07-07 11:02:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="centered")
 st.markdown("""
     <style>
     .block-container {
-        max-width: 1000px;
+        max-width: 900px;
         padding-top: 2rem;
         padding-right: 2rem;
         padding-left: 2rem;
